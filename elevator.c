b60bef53ffc3c8d2270049945f954e7fccf739f4
William Bellity <william.bellity1@gmail.com>
2021-09-01 13:06:23 +0200

    exercise done

--- elevator.c
+++ elevator.c
@@ -24,35 +24,80 @@ Building *create_building(int nbFloor, Elevator *elevator, PersonList **waitingL
 
 PersonList* exitElevator(Elevator *e) {
     if (e->persons != NULL) {
-    PersonList *cursor = e->persons;
-    for (int i = 0; i < e->capacity; i++) {
-        if (cursor->person->dest == e->currentFloor)
+        PersonList* cursor = e->persons;
+       
+        if ( cursor->person->dest == e->currentFloor)
         {
-            cursor = delete_start(cursor);
-        } else {
-            cursor = cursor->next;
-        }
+            e->persons = delete_start(e->persons); // Changed head    
+        } 
+        while(cursor->next != NULL)
+          {
+              //if yes, we need to delete the cursor->next node
+              if(cursor->next->person->dest == e->currentFloor)
+              {
+                  //node will be disconnected from the linked list.
+                  cursor->next = cursor->next->next;
+                  
+              }
+              //Otherwise, move the cursor node and proceed
+              else
+                  cursor = cursor->next;
+          }
+
+        
+        // if ( cursor->person->dest == e->currentFloor)
+        // {
+        //     e->persons = delete_end(e->persons); // Changed head    
+        // }
+        // PersonList* prev = NULL;
+    
+        
+        
+        // while (cursor != NULL && cursor->person->dest != e->currentFloor)
+        // {
+        //     prev = cursor;
+        //     cursor = cursor->next;
+        // }
+ 
+        // // If key was not present in linked list
+        // if (cursor == NULL)
+        //     return;
+ 
+        // // Unlink the node from linked list
+        // prev->next = cursor->next;
+ 
+    
+    
+        
+
+        // e->persons = delete_start(e->persons);
+        
+    } else {
+    return NULL;
     }
-    }
-    return e->persons;
 
 }
 
 PersonList* enterElevator(Elevator *e, PersonList *list) {
-    // int empty_places = len(e->persons);
-    // int len_waitingList = len(list);
+    int empty_places = e->capacity - len(e->persons);
+    int len_waitingList = len(list);
    
     PersonList *newWaitingList = list;
 
-    // for (int i = 0; i < len_waitingList && empty_places > 0; i++) {
-    //     e->persons = insert_start(cursor->person, e->persons);
-    //     cursor = cursor->next;
-    //     empty_places--;
-    //     cursor = delete_start(cursor);
-    // }
-    e->persons->person = newWaitingList->person;
-
+    for (int i = 0; i < len_waitingList && empty_places > 0; i++) {
+        
+        if (e->persons == NULL) {
+    e->persons = createPersonList(newWaitingList->person);
+    } else {
+        e->persons = insert_start(newWaitingList->person, e->persons);
+    }
     newWaitingList = delete_start(newWaitingList);
+    empty_places--;
+
+    }
+    
+
+    
     
    
 
@@ -63,8 +108,17 @@ PersonList* enterElevator(Elevator *e, PersonList *list) {
 void stepElevator(Building *b) {
     if (b->elevator->targetFloor != b->elevator->currentFloor) {
         b->elevator->currentFloor = b->elevator->targetFloor;
-        // b->elevator->persons = exitElevator(b->elevator);
-        b->waitingLists[b->elevator->targetFloor] = enterElevator(b->elevator, b->waitingLists[b->elevator->targetFloor]);
+        PersonList* personsOut = NULL;
+        
+        for (int i = 0; i <= b->elevator->capacity; i++) {
+            personsOut = exitElevator(b->elevator);
+            b->waitingLists[b->elevator->targetFloor] = enterElevator(b->elevator, b->waitingLists[b->elevator->targetFloor]);
+        }
+        
+        
+        
+
+
     } 
     
 
